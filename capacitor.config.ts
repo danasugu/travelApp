371b70834e12a52685bd78e58ed3ca8f6d249ed2
Dana Sugu <dana.sugu@gmail.com>
2021-08-09 18:15:26 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.danasugu.cistourro',
+  appName: 'CisTourRo',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
